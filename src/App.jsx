c76d4cc2f4fd89f127129a52313581511579c12f
mishasro72 <mishasro72@gmail.com>
2026-04-28 19:23:59 -0400

    Add addTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import { useState } from "react";
 
-const todos = [
-  { id: 1, title: "review resources" },
-  { id: 2, title: "take notes" },
-  { id: 3, title: "code out app" },
-];
-
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todoTitle) {
+    const todoItem = { id: Date.now(), title: todoTitle };
+
+    setTodoList((previos) => [...previos, todoItem]);
+  }
 
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </div>
   );

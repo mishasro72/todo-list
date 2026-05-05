062509f434d0bd2e8ed0c4e5c0e0d626e688bd21
mishasro72 <mishasro72@gmail.com>
2026-05-05 19:05:10 -0400

    Add a controlled form component, allow users to complete a todo by checking a checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,27 @@ function App() {
   const [todoList, setTodoList] = useState([]);
 
   function addTodo(todoTitle) {
-    const todoItem = { id: Date.now(), title: todoTitle };
+    const todoItem = { id: Date.now(), title: todoTitle, isCompleted: false };
 
-    setTodoList((previos) => [...previos, todoItem]);
+    setTodoList((previous) => [...previous, todoItem]);
+  }
+
+  function completeTodo(id) {
+    const updatedTodo = todoList.map((todo) => {
+      if (todo.id === id) {
+        return (todo = { ...todo, isCompleted: true });
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodo);
   }
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }

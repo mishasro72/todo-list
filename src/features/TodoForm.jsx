dd255cc15ceb78d7e075892b9a3308deeb00d9d6
mishasro72 <mishasro72@gmail.com>
2026-05-12 20:40:24 -0400

    Add custom hook, helper function, and re-organized project

---
+++ src/features/TodoForm.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useRef, useState } from "react";
+import TextInputWithLabel from "../shared/TextInputWithLabel";
+import { isValidTodoTitle } from "../utils/todoValidation";
+
+function TodoForm({ onAddTodo }) {
+  const inputRef = useRef();
+  const [workingTodoTitle, setWorkingTodoTitle] = useState("");
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+
+    onAddTodo(workingTodoTitle);
+    setWorkingTodoTitle("");
+    inputRef.current.focus();
+  }
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <TextInputWithLabel
+        elementId="todoTitle"
+        labelText="Todo"
+        onChange={(e) => setWorkingTodoTitle(e.target.value)}
+        value={workingTodoTitle}
+        ref={inputRef}
+      />
+      <button type="submit" disabled={!isValidTodoTitle(workingTodoTitle)}>
+        Add Todo
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
---
+++ src/features/TodoList/TodoListItem.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel";
+import { isValidTodoTitle } from "../../utils/todoValidation";
+import { useEditableTitle } from "../../hooks/useEditableTitle";
+
+export default function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  const {
+    isEditing,
+    workingTitle,
+    startEditing,
+    cancelEdit,
+    updateTitle,
+    finishEdit,
+  } = useEditableTitle(todo.title);
+
+  function handleCancel() {
+    cancelEdit();
+  }
+
+  function handleEdit(event) {
+    const value = event.target.value;
+    updateTitle(value);
+  }
+
+  function handleUpdate(event) {
+    event.preventDefault();
+    if (!isEditing || !isValidTodoTitle(workingTitle)) return;
+    const finalTitle = finishEdit();
+    onUpdateTodo({ ...todo, title: finalTitle });
+  }
+
+  return (
+    <li style={{ listStyleType: "none", padding: 0 }}>
+      <form onSubmit={handleUpdate}>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button
+              type="button"
+              onClick={handleUpdate}
+              disabled={!isValidTodoTitle(workingTitle)}
+            >
+              Update
+            </button>
+          </>
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                id={`checkbox${todo.id}`}
+                checked={todo.isCompleted}
+                onChange={() => onCompleteTodo(todo.id)}
+              />
+              <span onClick={() => startEditing()}>{todo.title}</span>
+            </label>
+          </>
+        )}
+      </form>
+    </li>
+  );
+}

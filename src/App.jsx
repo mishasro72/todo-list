a5c1c0a59ea8257a2d95da9c33f22703d0db045f
mishasro72 <mishasro72@gmail.com>
2026-06-09 17:41:34 -0400

    Implement Route Protection, add navigate component, update logoff

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AboutPage from "./pages/AboutPage";
 import { Route, Routes } from "react-router";
 import ProfilePage from "./pages/ProfilePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import RequireAuth from "./components/RequireAuth";
 
 function App() {
   return (
@@ -15,12 +16,25 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/todos" element={<TodosPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/todos"
+          element={
+            <RequireAuth>
+              <TodosPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <RequireAuth>
+              <ProfilePage />{" "}
+            </RequireAuth>
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
-
       </Routes>
     </>
   );
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { useAuth } from "../context/AuthContext";
 import Logoff from "../features/Logoff";
 import { Link } from "react-router";
+import Navigation from "./Navigation";
 
 export default function Header() {
   const { isAuthenticated } = useAuth();
   return (
     <div>
       <h1>Todo List</h1>
-      <Link to="/profile">Profile</Link>
-      <br/>
       <Link to="fgfgfg">404 error test link</Link>
+      <Navigation />
       {isAuthenticated && (
         <div>
           <span>Logon</span>

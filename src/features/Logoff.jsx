3893fb0036b7a500dbe2555662ca488a33a8ff8d
mishasro72 <mishasro72@gmail.com>
2026-06-17 00:56:35 -0400

    Remove console.error in logoff component

--- src/features/Logoff.jsx
+++ src/features/Logoff.jsx
@@ -9,8 +9,6 @@ export default function Logoff() {
     const result = await logout();
     if (result.success) {
       navigate("/login");
-    } else {
-      console.error(result.error);
     }
   };
 
